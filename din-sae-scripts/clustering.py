520421a6762f42e6d5c57474a0a7b6c63cb2d4a9
John Chrosniak <chrosniakj@gmail.com>
2024-12-09 16:45:01 -0500

    added logic to continue to iterate kmeans until all points belong to a cluster

--- din-sae-scripts/clustering.py
+++ din-sae-scripts/clustering.py
@@ -8,17 +8,28 @@ def main(num_points, timesteps):
     cluster_centroids = get_largest_frame(points)
     flattened_points = np.vstack(points)
     kmeans = determine_clusters(cluster_centroids, flattened_points, len(cluster_centroids))
-    radius = determine_cluster_radius(flattened_points, kmeans, 0)
-    clusters_passed = 0
-    radius_shrink_iterations = 0
-    while clusters_passed != timesteps:
-        clusters_passed = 0
+    while True:
+        radius, _ = determine_cluster_radius(flattened_points, kmeans, 0)
+        radius_shrink_iterations = 0
+        restart = False
         for i in range(timesteps):
-            if check_clusters_are_valid(points[i], kmeans, radius):
-                clusters_passed += 1
-            else:
+            while not check_points_have_one_cluster(points[i], kmeans, radius):
                 radius_shrink_iterations += 1
-                radius = determine_cluster_radius(flattened_points, kmeans, radius_shrink_iterations)
+                radius, furthest_point = determine_cluster_radius(flattened_points, kmeans, radius_shrink_iterations)
+        for i in range(timesteps):
+            if not check_all_points_have_cluster(points[i], kmeans, radius):
+                new_centroids = np.vstack((kmeans.cluster_centers_, furthest_point))
+                kmeans = determine_clusters(new_centroids, flattened_points, len(new_centroids))
+                print("Adding centroid at (%f, %f) and re-running K-means" % (furthest_point[0], furthest_point[1]))
+                print("Current cluster radius is %f" % (radius))
+                print("Now there are %d centroids" % (len(kmeans.cluster_centers_)))
+                restart = True
+                break
+        if not restart:
+            break
+    for i in range(timesteps):
+        assert(check_points_have_one_cluster(points[i], kmeans, radius))
+        assert(check_all_points_have_cluster(points[i], kmeans, radius))
     fig, ax = plt.subplots(1, timesteps)
     for i in range(timesteps):
         ax[i].plot(points[i][:,0], points[i][:,1], 'b.')
@@ -26,9 +37,13 @@ def main(num_points, timesteps):
         for j in range(len(kmeans.cluster_centers_)):
             circle = Circle(kmeans.cluster_centers_[j], radius=radius, fill=False)
             ax[i].add_patch(circle)
-    for a in ax:
-        a.set_xlim([-0.1, 2.1])
-        a.set_ylim([-0.1, 2.1])
+    for i in range(len(ax)):
+        a = ax[i]
+        a.set_xlim([0, 2])
+        a.set_ylim([0, 2])
+        a.set_xlabel("Azimuth (m)")
+        a.set_ylabel("Elevation (m)")
+        a.set_title("Time: %d" % (i))
         a.set_aspect('equal')
     plt.show()
 
@@ -39,7 +54,7 @@ Generate a series of simulated lidar points on a plane within a specified range
 def generate_simulated_points(num_points : int, 
                            timesteps: int,
                            square_range : tuple,
-                           dropout_probability=0.05):
+                           dropout_probability=0.1):
     points = []
     # Get initial points
     points.append(np.random.uniform(square_range[0], square_range[1], size=(num_points, 2)))
@@ -74,17 +89,21 @@ def determine_clusters(centroids, points, num_clusters):
 
 def determine_cluster_radius(points, kmeans, iteration):
     distances = np.zeros(len(points))
+    max_distance = 0.0
+    furthest_point = None
     for i in range(len(points)):
         assigned_cluster = kmeans.labels_[i]
         distances[i] = np.linalg.norm(points[i] - kmeans.cluster_centers_[assigned_cluster])
+        if distances[i] > max_distance:
+            max_distance = distances[i]
+            furthest_point = points[i]
     distances.sort()
-    return distances[-1 - iteration]
+    return distances[-1 - iteration], furthest_point
 
 """
-By definition of K-Means points will always be assigned to a cluster
 Need to ensure that no points belong to two clusters
 """
-def check_clusters_are_valid(points, kmeans, radius):
+def check_points_have_one_cluster(points, kmeans, radius):
     for i in range(len(points)):
         num_clusters_belonging = 0
         for j in range(len(kmeans.cluster_centers_)):
@@ -92,6 +111,17 @@ def check_clusters_are_valid(points, kmeans, radius):
             if np.linalg.norm(points[i] - kmeans.cluster_centers_[j]) <= radius:
                 num_clusters_belonging += 1
         if num_clusters_belonging > 1:
+            return False
+    return True
+
+def check_all_points_have_cluster(points, kmeans, radius):
+    for i in range(len(points)):
+        num_clusters_belonging = 0
+        for j in range(len(kmeans.cluster_centers_)):
+            # If the point falls within the radius of a point that is not its assigned cluster, the clustering is not valid
+            if np.linalg.norm(points[i] - kmeans.cluster_centers_[j]) <= radius:
+                num_clusters_belonging += 1
+        if num_clusters_belonging == 0:
             return False
     return True
 
